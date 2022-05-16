349f192a4710872126dd2a40c9a410644dd33aed
JiiXaa <anythingfx@gmail.com>
2022-05-16 18:17:51 +0100

    logic for parsing form data when post '/'

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const PORT = 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(' Response Testing');
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email"/>
+      <input type="password" name="password" placeholder="password"/>
+      <input type="password" name="passwordConfirmation" placeholder="confirm password"/>
+      <button>Sign Up</button>
+    </form>
+  </div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  req.on('data', (data) => {
+    const parsed = data.toString('utf8').split('&');
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split('=');
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+  res.send('Account created! ');
 });
 
 app.listen(PORT, () => {
