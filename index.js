a68cb101f1d5beed9a7513fd008554d0e0be5472
JiiXaa <anythingfx@gmail.com>
2022-05-26 20:31:51 +0100

    sign-in and sign-out functionality

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
   <div>
     Your id is: ${req.session.userId}
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -50,6 +50,41 @@ app.post('/', async (req, res) => {
   req.session.userId = user.id;
 
   res.send('Account created!');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email"/>
+      <input type="password" name="password" placeholder="password"/>
+      <button>Sign In</button>
+    </form>
+  </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  if (user.password !== password) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in!');
 });
 
 app.listen(PORT, () => {
