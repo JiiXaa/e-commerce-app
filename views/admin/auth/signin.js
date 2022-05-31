a8bcf13ae8ceef9318f2e1caaf78f41b73d553e3
JiiXaa <anythingfx@gmail.com>
2022-05-31 19:18:46 +0100

    layout template for displaying HTML tree in multiple routes

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,15 @@
+const layout = require('../layout');
+
 module.exports = () => {
-  return `
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email"/>
-        <input type="password" name="password" placeholder="password"/>
-        <button>Sign In</button>
-      </form>
-    </div>
-  `;
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="email"/>
+          <input type="password" name="password" placeholder="password"/>
+          <button>Sign In</button>
+        </form>
+      </div>
+    `,
+  });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-  return `
-    <div>
-      Your id is: ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="email"/>
-        <input type="password" name="password" placeholder="password"/>
-        <input type="password" name="passwordConfirmation" placeholder="confirm password"/>
-        <button>Sign Up</button>
-      </form>
-  </div>
-  `;
+  return layout({
+    content: `
+      <div>
+        Your id is: ${req.session.userId}
+        <form method="POST">
+          <input name="email" placeholder="email"/>
+          <input type="password" name="password" placeholder="password"/>
+          <input type="password" name="passwordConfirmation" placeholder="confirm password"/>
+          <button>Sign Up</button>
+        </form>
+      </div>
+    `,
+  });
 };
