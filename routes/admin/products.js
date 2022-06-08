91f8fa01753e675c207c9e70ef47b8c7a5ead2d1
JiiXaa <anythingfx@gmail.com>
2022-06-08 20:35:04 +0100

    admin form template for adding new products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
 
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {});
-router.get('/admin/products/new', (req, res) => {});
+
+router.get('/admin/products/new', (req, res) => {
+  // passing empty {} to avoid error.
+  res.send(productsNewTemplate({}));
+});
 
 module.exports = router;
