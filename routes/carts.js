aed5380f87f3e4a6c2c9e06c32c115246baa835f
JiiXaa <anythingfx@gmail.com>
2022-06-25 14:04:09 +0100

    add item to the cart logic

--- routes/carts.js
+++ routes/carts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Receive a POST request to add an item to a cart
 router.post('/cart/products', async (req, res) => {
   // Figure out if user has cart
-  // req.session is managed by cookie seesion
+  // req.session is managed by cookie session
   let cart; // we create variable out of scope of if statement so its not scoped to the if block
   if (!req.session.cartId) {
     // User does not have a cart, we need to create one, and store the cart id on the req.session.cartId property
@@ -16,10 +16,24 @@ router.post('/cart/products', async (req, res) => {
     // User have a cart already, Let's get it from the repository
     cart = await cartsRepo.getOne(req.session.cartId);
   }
-
   console.log(cart);
   // Either increment quantity for existing product
   // OR add new product to items array
+  const existingProduct = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+
+  if (existingProduct) {
+    // increment quantity and save cart
+    existingProduct.quantity++;
+  } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send('Product added to your cart');
 });
